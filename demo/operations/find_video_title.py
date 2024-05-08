5dac9d29acf1d2b5010d407b7fb5959b3365f8ef
504311708@qq.com <504311708@qq.com>
2024-05-08 20:51:45 +0800

    小修改，推荐视频顺序为权重降序

--- demo/operations/find_video_title.py
+++ demo/operations/find_video_title.py
@@ -1,13 +1,16 @@
 import csv
-
+import pandas as pd
 def find_video_title(video_title_path, rec_res):
   res = []
   try:
-    with open(video_title_path, newline="", encoding="utf-8-sig") as file:
-      reader = csv.reader(file)
-      for index,row in enumerate(reader):
-        if index in rec_res:
-          res.append([str(index), row[1]])
+    titles = pd.read_csv(video_title_path, usecols=['title'])
+    for rec_vid in rec_res:
+      res.append([str(rec_vid), titles.iat[rec_vid,0]])
+    # with open(video_title_path, newline="", encoding="utf-8-sig") as file:
+    #   reader = csv.reader(file)
+    #   for index,row in enumerate(reader):
+    #     if index in rec_res:
+    #       res.append([str(index), row[1]])
     return res
   except:
     print("寻找视频标题目录失败")
